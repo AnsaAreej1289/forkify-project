cc4553998c505ff226a52e3b0bae99c8c2d78acd
Ansa Areej <gulpashareej845@gmail.com>
2026-09-03 11:52:02 +0500

    Add Forkify API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '5b0a644a-7ece-46a8-8407-68f88bd59f4d';
 export const MODAL_CLOSE_SEC = 2.5;
