7cb697967e9d749d89c41e2a1a77c75be61fe8d9
Ansa Areej <gulpashareej845@gmail.com>
2026-09-03 10:27:56 +0500

    Update controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -118,6 +118,9 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err.message);
   }
 };
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
 
 const init = function () {
  
@@ -128,6 +131,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
+newFeature();
 };
 init();
